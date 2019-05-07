7075f945f42754f9efd658a191eb2669c5f4cff1
reza rahman <rezarahman795@gmail.com>
2019-05-07 12:34:15 +0700

    push data baru

--- register.php
+++ register.php
@@ -26,10 +26,10 @@
        <input type="submit" name="load_data" value="Load Data" />
  </form>
  <?php
-    $host = "<Nama server database Anda>";
-    $user = "<Nama admin database Anda>";
-    $pass = "<Password admin database Anda>";
-    $db = "<Nama database Anda>";
+    $host = "azuresub1reza.database.windows.net";
+    $user = "AZURE_REZA";
+    $pass = "AJINbutak98";
+    $db = "AzureSub1Reza";
     try {
         $conn = new PDO("sqlsrv:server = $host; Database = $db", $user, $pass);
         $conn->setAttribute( PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION );
